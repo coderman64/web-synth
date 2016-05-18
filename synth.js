f4b0bcb7d2beb1f0198a4b4cd8283de29a50a1cc
coderman64 <coderman64@users.noreply.github.com>
2016-05-18 09:47:21 -0400

    Create synth.js

---
+++ synth.js
@@ -0,0 +1,91 @@
+
+var getFreq = function(keyNumber){
+    return Math.pow(2,((keyNumber-49)/12))*440;
+}
+
+var context = new AudioContext(),
+    masterVolume = context.createGain(),
+    oscillators = {};
+
+masterVolume.gain.value = 0.2;
+
+masterVolume.connect(context.destination);
+
+var canvas = document.getElementById("mainCanvas");
+var cont = document.getElementById("keyboard");
+var c = canvas.getContext("2d");
+c.fillStyle = "#FF0000";
+//c.fillRect(0,0,150,75);
+c.strokeStyle = "#000000";
+for(var i = 0; i < 20;i++){
+    c.moveTo(0,i*10);
+    c.lineTo(500,i*10);
+    c.stroke();
+}
+var mouseLoc = {
+       x: 0,
+       y: 0
+    }; 
+var mouseDown = false;
+canvas.addEventListener("mousedown", function(evt){
+    mouseDown = true;
+    c.moveTo(mouseLoc.x,mouseLoc.y);
+    var osc = context.createOscillator();
+    var frequency = getFreq(Math.floor(mouseLoc.y/10)+30);
+    c.fillStyle = "#000000";
+    c.fillRect(mouseLoc.x,Math.floor(mouseLoc.y/10)*10,50,10);
+    c.stroke();
+    osc.frequency.value = frequency;
+    osc.type = 'triangle';
+    osc.connect(masterVolume);
+    oscillators[frequency] = [osc];
+    osc.start(context.currentTime);
+    osc.stop(context.currentTime+0.2);
+});
+
+canvas.addEventListener("mouseup", function(evt){
+    mouseDown = false;
+});
+
+canvas.addEventListener("mousemove",function(evt){
+    var rect = canvas.getBoundingClientRect();
+    mouseLoc = {
+       x: evt.clientX - rect.left,
+       y: evt.clientY - rect.top
+    }; 
+    cont.innerHTML = "Key No. "+(Math.floor(mouseLoc.y/10)+30).toString();
+    if(mouseDown){
+        //c.lineTo(mouseLoc.x,mouseLoc.y);
+        //c.stroke();
+        
+    }
+});
+
+/*keyboard.keyDown = function (note, frequency) {
+    var osc = context.createOscillator(),
+        osc2 = context.createOscillator();
+
+    osc.frequency.value = frequency;
+    osc.type = 'triangle';
+    osc.detune.value = -10;
+
+    osc2.frequency.value = frequency;
+    osc2.type = 'triangle';
+    osc2.detune.value = 10;
+
+    osc.connect(masterVolume);
+    osc2.connect(masterVolume);
+
+    masterVolume.connect(context.destination);
+
+    oscillators[frequency] = [osc, osc2];
+
+    osc.start(context.currentTime);
+    osc2.start(context.currentTime);
+};
+
+keyboard.keyUp = function (note, frequency) {
+    oscillators[frequency].forEach(function (oscillator) {
+        oscillator.stop(context.currentTime);
+    });
+};*/
