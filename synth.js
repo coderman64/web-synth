7538dbab8ffd08ce6353811f120987cb13c5691e
coderman64 <coderman64@users.noreply.github.com>
2016-05-20 13:22:14 -0400

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,3 @@
-
 var getFreq = function(keyNumber){
     return Math.pow(2,((keyNumber-49)/12))*440;
 }
@@ -22,6 +21,20 @@ for(var i = 0; i < 20;i++){
     c.lineTo(500,i*10);
     c.stroke();
 }
+
+var notes = [];
+var note = function(key, time, length){
+    this.key = key;
+    this.time = time;
+    this.length = length;
+}
+note.prototype.draw = function(){
+    //(key-30)*10
+    c.fillStyle = "#000000";
+    c.fillRect(this.time,(this.key-30)*10, this.length, 10);
+    c.stroke();
+}
+
 var mouseLoc = {
        x: 0,
        y: 0
@@ -32,9 +45,11 @@ canvas.addEventListener("mousedown", function(evt){
     c.moveTo(mouseLoc.x,mouseLoc.y);
     var osc = context.createOscillator();
     var frequency = getFreq(Math.floor(mouseLoc.y/10)+30);
-    c.fillStyle = "#000000";
-    c.fillRect(mouseLoc.x,Math.floor(mouseLoc.y/10)*10,50,10);
-    c.stroke();
+    //c.fillStyle = "#000000";
+    //c.fillRect(mouseLoc.x,Math.floor(mouseLoc.y/10)*10,50,10);
+    //c.stroke();
+    notes[notes.length] = new note(Math.floor(mouseLoc.y/10)+30, mouseLoc.x, 10);
+    notes[notes.length-1].draw();
     osc.frequency.value = frequency;
     osc.type = 'triangle';
     osc.connect(masterVolume);
@@ -64,26 +79,19 @@ canvas.addEventListener("mousemove",function(evt){
 /*keyboard.keyDown = function (note, frequency) {
     var osc = context.createOscillator(),
         osc2 = context.createOscillator();
-
     osc.frequency.value = frequency;
     osc.type = 'triangle';
     osc.detune.value = -10;
-
     osc2.frequency.value = frequency;
     osc2.type = 'triangle';
     osc2.detune.value = 10;
-
     osc.connect(masterVolume);
     osc2.connect(masterVolume);
-
     masterVolume.connect(context.destination);
-
     oscillators[frequency] = [osc, osc2];
-
     osc.start(context.currentTime);
     osc2.start(context.currentTime);
 };
-
 keyboard.keyUp = function (note, frequency) {
     oscillators[frequency].forEach(function (oscillator) {
         oscillator.stop(context.currentTime);
