4c30da6c9498512084b9f6b45e8a3835c8f51caa
Dylan Bruss <Dylan Bruss>
2016-06-09 13:43:44 -0400

    bug fixes/ added different cursors

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ var newSynthButton = document.getElementById("newSynth");
 newSynthButton.addEventListener("click",function(){
     newSynthWindow();
 });
+
+var partsCanvas = document.getElementById("partsCanvas");
+var rect = partsCanvas.getBoundingClientRect();
+partsCanvas.width = window.innerWidth;
+partsCanvas.height = window.innerHeight-rect.top;
+partsCanvas.position = "absoulte";
+partsCanvas.style.left = "0px";
--- synth.js
+++ synth.js
@@ -37,35 +37,46 @@ var getFreq = function(keyNumber){
     }; 
 var mouseDown = false;*/
 /***************************************************************************************** */
-var synthApp = function(canvas1, cont1, playbutton1){
+var synthApp = function(canvas1, cont1, playbutton1, part){
     this.canvas = canvas1;
     this.cont = cont1;
     this.c = this.canvas.getContext("2d");
     this.rightButton = false;
-    this.notes = [];
+    if(part){
+        this.notes = part;
+    }else{
+        this.notes = [];
+    }
     this.interval = null;
     this.play = 0;
     this.playbutton = playbutton1;
     this.mouseLoc = {x:0, y:0};
     this.mouseDown = false;
-    
+    this.resize = -1;
 }
 
 synthApp.prototype.mouseDown1 = function(evt){
     this.mouseDown = true;
     if(evt.button === 0){
-    var osc = context.createOscillator();
-    var frequency = getFreq(Math.floor(this.mouseLoc.y/10));
-    //c.fillStyle = "#000000";
-    //c.fillRect(mouseLoc.x,Math.floor(mouseLoc.y/10)*10,50,10);
-    //c.stroke();
-    this.notes[this.notes.length] = new note(Math.floor(this.mouseLoc.y/10), Math.round(this.mouseLoc.x/5)*5, 10);
-    this.notes[this.notes.length-1].draw(this.c);
-    osc.frequency.value = frequency;
-    osc.type = 'triangle';
-    osc.connect(masterVolume);
-    osc.start(context.currentTime);
-    osc.stop(context.currentTime+0.2);
+        for(var i = 0; i<this.notes.length; i++){
+            var noteY = (this.notes[i].key)*10
+            if(this.mouseLoc.x > this.notes[i].time&&this.mouseLoc.x<(this.notes[i].time+this.notes[i].length)&&this.mouseLoc.y>noteY&&this.mouseLoc.y<noteY+10){
+                    this.notes[i].length = Math.ceil((this.mouseLoc.x-this.notes[i].time)/5)*5;
+                    this.resize = i;
+            }
+        }
+        if(this.resize == false){
+            var osc = context.createOscillator();
+            var frequency = getFreq(Math.floor(this.mouseLoc.y/10));
+            this.notes[this.notes.length] = new note(Math.floor(this.mouseLoc.y/10), Math.round(this.mouseLoc.x/5)*5, 10);
+            this.notes[this.notes.length-1].draw(this.c);
+            osc.frequency.value = frequency;
+            osc.type = 'triangle';
+            osc.connect(masterVolume);
+            osc.start(context.currentTime);
+            osc.stop(context.currentTime+0.2);
+        }
+        this.canvas.style.cursor = "col-resize";
     }else if(evt.button == 2){
         this.rightButton = true;
     }
@@ -73,9 +84,11 @@ synthApp.prototype.mouseDown1 = function(evt){
 
 synthApp.prototype.mouseUp1 = function(evt){
     this.mouseDown = false;
+    this.resize = -1;
     if(evt.button == 2){//if the right button is pressed
         this.rightButton = false;
     }
+    this.canvas.style.cursor = "default";
 };
 
 synthApp.prototype.mouseMove1 = function(evt){
@@ -86,7 +99,9 @@ synthApp.prototype.mouseMove1 = function(evt){
     }; //change the "mouseLoc" variable to reflect the mouse's current position
     this.cont.innerHTML = "Key No. "+(Math.floor(this.mouseLoc.y/10)).toString();
     if(this.mouseDown && evt.button == 0&& this.rightButton == false){
-        if(this.notes[this.notes.length-1].time<this.mouseLoc.x){
+        if(this.resize>-1){
+            this.notes[this.resize].length = Math.round((this.mouseLoc.x-this.notes[this.resize].time)/5)*5;
+        }else if(this.notes[this.notes.length-1].time<this.mouseLoc.x){
             this.notes[this.notes.length-1].length = Math.round((this.mouseLoc.x-this.notes[this.notes.length-1].time)/5)*5;//allows you to drag out the length of the note
         }
     }
@@ -111,6 +126,10 @@ synthApp.prototype.redraw = function(){
             this.c.lineWidth = 2;
         }else{
             this.c.lineWidth = 1;
+        }
+        
+        if(this.mouseLoc.x > this.notes[i].time&&this.mouseLoc.x<(this.notes[i].time+this.notes[i].length)&&this.mouseLoc.y>noteY&&this.mouseLoc.y<noteY+10){
+            this.notes[i].length = Math.ceil((this.mouseLoc.x-this.notes[i].time)/5)*5;
         }
         this.c.strokeStyle = "#000000";
         this.c.moveTo(i*20,0);
@@ -145,9 +164,15 @@ synthApp.prototype.redraw = function(){
             this.notes[i].osc.stop(context.currentTime);
         }
         var noteY = (this.notes[i].key)*10
-        if(this.rightButton&&this.mouseLoc.x > this.notes[i].time&&this.mouseLoc.x<(this.notes[i].time+this.notes[i].length)&&this.mouseLoc.y>noteY&&this.mouseLoc.y<noteY+10){
-            this.notes.splice(i,1);
-            i--;
+        if(this.mouseLoc.x > this.notes[i].time&&this.mouseLoc.x<(this.notes[i].time+this.notes[i].length)&&this.mouseLoc.y>noteY&&this.mouseLoc.y<noteY+10){
+                if(this.rightButton){
+                    this.notes.splice(i,1);
+                    i--;
+                }else if(this.mouseDown == false){
+                    this.canvas.style.cursor = "w-resize";
+                }
+        }else if(this.canvas.style.cursor == "w-resize"){
+            this.canvas.style.cursor = "default";
         }
     }
     if(this.play > 0){
@@ -159,7 +184,7 @@ synthApp.prototype.redraw = function(){
         this.c.stroke();
         if(this.play>this.canvas.width){
             this.play = 0;
-            this.playbutton.innerHTML = "Play!"
+            this.playbutton.innerHTML = "Play!";
         }
     }
 };
@@ -175,143 +200,3 @@ function newSynth(canvas1, cont1, playbutton1){
     return temp12;
 }
 /****************************************************************************************************** */
-/*function redraw(){
-    c.beginPath();
-    c.fillStyle = "#FFFFFF";
-    c.fillRect(0,0,canvas.width,canvas.height);
-    for(var i = 0; i < Math.max(Math.floor(canvas.width/20),Math.floor(canvas.height/10));i++){
-        if(i/4==Math.floor(i/4)){
-            c.lineWidth = 2;
-        }else{
-            c.lineWidth = 1;
-        }
-        c.strokeStyle = "#000000";
-        c.moveTo(i*20,0);
-        c.lineTo(i*20,canvas.height);
-        c.stroke();
-        c.beginPath();
-        c.strokeStyle = "#000000";
-        if((i-6)/12==Math.floor((i-6)/12)){
-            c.lineWidth = 2;
-        }else{
-            c.lineWidth = 1;
-        }
-        c.moveTo(0,i*10);
-        c.lineTo(canvas.width,i*10);
-        c.stroke();
-        c.beginPath();
-    }
-    for(var i = 0; i < notes.length; i++){
-        notes[i].draw();
-        if(play>notes[i].time&&play<notes[i].time+6){
-            if(notes[i].osc){
-                notes[i].osc.stop(context.currentTime);
-            }
-            notes[i].osc = context.createOscillator();
-            var frequency = getFreq(notes[i].key);
-            notes[i].osc.frequency.value = frequency;
-            notes[i].osc.type = 'triangle';
-            notes[i].osc.connect(masterVolume);
-            notes[i].osc.start(context.currentTime);
-        }
-        if(notes[i].osc&&((play>(notes[i].time+notes[i].length)&&play<(notes[i].time+notes[i].length)+6)||play < 1)){
-            notes[i].osc.stop(context.currentTime);
-        }
-        var noteY = (notes[i].key)*10
-        if(rightButton&&mouseLoc.x > notes[i].time&&mouseLoc.x<(notes[i].time+notes[i].length)&&mouseLoc.y>noteY&&mouseLoc.y<noteY+10){
-            notes.splice(i,1);
-            i--;
-        }
-    }
-    if(play > 0){
-        c.beginPath();
-        play += 5;
-        c.strokeStyle = "#FF0000";
-        c.moveTo(play,0);
-        c.lineTo(play,canvas.height);
-        c.stroke();
-        if(play>canvas.width){
-            play = 0;
-            playbutton.innerHTML = "Play!"
-        }
-    }
-}
-
-canvas.addEventListener("mousedown", function(evt){
-    mouseDown = true;
-    if(evt.button === 0){
-    c.moveTo(mouseLoc.x,mouseLoc.y);
-    var osc = context.createOscillator();
-    var frequency = getFreq(Math.floor(mouseLoc.y/10));
-    //c.fillStyle = "#000000";
-    //c.fillRect(mouseLoc.x,Math.floor(mouseLoc.y/10)*10,50,10);
-    //c.stroke();
-    notes[notes.length] = new note(Math.floor(mouseLoc.y/10), Math.round(mouseLoc.x/5)*5, 10);
-    notes[notes.length-1].draw();
-    osc.frequency.value = frequency;
-    osc.type = 'triangle';
-    osc.connect(masterVolume);
-    osc.start(context.currentTime);
-    osc.stop(context.currentTime+0.2);
-    }else if(evt.button == 2){
-        rightButton = true;
-    }
-});
-
-canvas.addEventListener("mouseup", function(evt){
-    mouseDown = false;
-    if(evt.button == 2){//if the right button is pressed
-        rightButton = false;
-    }
-});
-
-var moved12 = function(evt){
-    var rect = canvas.getBoundingClientRect();
-    mouseLoc = {
-       x: evt.clientX - rect.left,
-       y: evt.clientY - rect.top
-    }; //change the "mouseLoc" variable to reflect the mouse's current position
-    cont.innerHTML = "Key No. "+(Math.floor(mouseLoc.y/10)).toString();
-    if(mouseDown && evt.button == 0&& rightButton == false){
-        if(notes[notes.length-1].time<mouseLoc.x){
-            notes[notes.length-1].length = Math.round((mouseLoc.x-notes[notes.length-1].time)/5)*5;//allows you to drag out the length of the note
-        }
-    }
-}
-canvas.addEventListener("mousemove",moved12);
-canvas.addEventListener("touchmove",function(evt){evt.preventDefault(); moved12(evt); console.log(evt.type)});
-
-playbutton.addEventListener("click",function(){
-    if(play>0){
-        play = 0;
-        playbutton.innerHTML = "Play!";//stops playback and changes the button text to "Play!"
-    }else{
-        play = 1;
-        playbutton.innerHTML = "Stop!";//begins playback and changes the button text to "Stop!"
-    }
-});
-
-canvas.oncontextmenu = function (e){
-    e.preventDefault(); //prevents the canvas from pulling up a menu when right-clicked
-}
-/*keyboard.keyDown = function (note, frequency) {
-    var osc = context.createOscillator(),
-        osc2 = context.createOscillator();
-    osc.frequency.value = frequency;
-    osc.type = 'triangle';
-    osc.detune.value = -10;
-    osc2.frequency.value = frequency;
-    osc2.type = 'triangle';
-    osc2.detune.value = 10;
-    osc.connect(masterVolume);
-    osc2.connect(masterVolume);
-    masterVolume.connect(context.destination);
-    oscillators[frequency] = [osc, osc2];
-    osc.start(context.currentTime);
-    osc2.start(context.currentTime);
-};
-keyboard.keyUp = function (note, frequency) {
-    oscillators[frequency].forEach(function (oscillator) {
-        oscillator.stop(context.currentTime);
-    });
-};*/
